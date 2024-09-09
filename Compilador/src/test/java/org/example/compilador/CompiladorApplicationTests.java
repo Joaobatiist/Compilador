6b8c512a0451cd596e665de64cb417fc8298e8bd
Joao victor Passos Batista de Oliveira <133144886+Joaobatiist@users.noreply.github.com>
2024-09-09 14:58:11 -0300

    .
    .

---
+++ Compilador/src/test/java/org/example/compilador/CompiladorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.compilador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompiladorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
