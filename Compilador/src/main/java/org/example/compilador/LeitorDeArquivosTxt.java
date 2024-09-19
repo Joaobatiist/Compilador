10ed53c67c5a0a1c72a5480080f873b96cb8e318
Gabriel Borges <bastosgbr@gmail.com>
2024-09-19 14:24:15 -0300

    Analisador Léxico Finalizado (parcial)

--- Compilador/src/main/java/org/example/compilador/LeitorDeArquivosTxt.java
+++ Compilador/src/main/java/org/example/compilador/LeitorDeArquivosTxt.java
@@ -1,29 +1,45 @@
 package org.example.compilador;
+
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LeitorDeArquivosTxt {
-    InputStream is;
+    private PushbackInputStream pushbackInputStream;
 
     public LeitorDeArquivosTxt(String arquivo) {
         try {
-            is = new FileInputStream(new File(arquivo));
+            FileInputStream fileInputStream = new FileInputStream(new File(arquivo));
+            pushbackInputStream = new PushbackInputStream(fileInputStream, 1); // O tamanho do buffer é 1
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(LeitorDeArquivosTxt.class.getName()).log(Level.SEVERE,null,ex);
+            Logger.getLogger(LeitorDeArquivosTxt.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public int lerProximoCaractere(){
 
+    public int lerProximoCaractere() {
         try {
-           int ret = is.read();
-            System.out.println((char) ret);
-            return  ret;
+            int ret = pushbackInputStream.read(); // Lê o próximo caractere
+            System.out.print((char) ret); // Exibe o caractere lido
+            return ret;
         } catch (IOException ex) {
-            Logger.getLogger(LeitorDeArquivosTxt.class.getName()).log(Level.SEVERE,null,ex);
-            return  -1;
+            Logger.getLogger(LeitorDeArquivosTxt.class.getName()).log(Level.SEVERE, null, ex);
+            return -1; // Retorna -1 em caso de erro ou fim do arquivo
         }
+    }
 
+    public void retroceder(int caractere) {
+        try {
+            pushbackInputStream.unread(caractere); // Retrocede o último caractere lido
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void close() {
+        try {
+            pushbackInputStream.close(); // Fecha o fluxo
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Compilador/src/main/java/org/example/compilador/LerToken.java
+++ Compilador/src/main/java/org/example/compilador/LerToken.java
@@ -1,4 +1,6 @@
 package org.example.compilador;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LerToken {
     LeitorDeArquivosTxt ldat;
@@ -8,278 +10,98 @@ public class LerToken {
     }
 
     public Token proximoToken() {
-        int caractereLido = -1;
+        int caractereLido;
+        StringBuilder palavra = new StringBuilder();
+
         while ((caractereLido = ldat.lerProximoCaractere()) != -1) {
             char c = (char) caractereLido;
-            if (c == ' ' || c == '\n') continue;
 
-            if (c == 'a') {
-                StringBuilder palavra = new StringBuilder();
+            // Ignorar espaços em branco
+            if (Character.isWhitespace(c)) continue;
+
+            // Identificando identificadores
+            if (Character.isLetter(c) || c == '_') {
                 palavra.append(c);
-
-
+                // Continuar a acumular caracteres enquanto forem válidos
                 while ((caractereLido = ldat.lerProximoCaractere()) != -1) {
                     c = (char) caractereLido;
-                    if (Character.isWhitespace(c)) {
-                        break;
+                    if (Character.isLetterOrDigit(c) || c == '_') {
+                        palavra.append(c);
+                    } else {
+                        ldat.retroceder(caractereLido); // método para retroceder um caractere
+                        break; // sair do loop
                     }
-                    palavra.append(c);
                 }
-                // identificadores
-
-                //DELIMITADORES
-             if (c == '(') {
-                return new Token(TipodeToken.PARENTESE_ABRINDO, "(");
-            }
-             if (c == ')') {
-                return new Token(TipodeToken.PARENTESE_FECHANDO, ")");
-            }
-             if (c == '{') {
-                return new Token(TipodeToken.CHAVES_ABRINDO, "{");
-            }
-             if (c == '}') {
-                return new Token(TipodeToken.CHAVES_FECHANDO, "}");
-            }
-             if (c == '[') {
-                return new Token(TipodeToken.CHAVES_ABRINDO, "[");
-            }
-             if (c == ']') {
-                return new Token(TipodeToken.CHAVES_FECHANDO, "]");
-            }
-              if (c == ';') {
-                 return new Token(TipodeToken.PONTO_VIRGULA, ";");
-             }
-                // Palvra reservada
-                if (palavra.toString().equals("abstract")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "abstract");
-                }
-                if (palavra.toString().equals("assert")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "assert");
-                }
-                if (palavra.toString().equals("boolean")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "boolean");
-                }
-                if (palavra.toString().equals("break")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "break");
-                }
-                if (palavra.toString().equals("case")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "case");
-                }
-                if (palavra.toString().equals("byte")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "byte");
-                }
-                if (palavra.toString().equals("catch")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "catch");
-                }
-                if (palavra.toString().equals("char")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "char");
-                }
-                if (palavra.toString().equals("class")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "class");
-                }
-                if (palavra.toString().equals("const")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "const");
-                }
-                if (palavra.toString().equals("continue")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "continue");
-                }
-                if (palavra.toString().equals("default")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "default");
-                }
-                if (palavra.toString().equals("do")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "do");
-                }
-                if (palavra.toString().equals("double")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "double");
-                }
-                if (palavra.toString().equals("else")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "else");
-                }
-                if (palavra.toString().equals("enum")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "enum");
-                }
-                if (palavra.toString().equals("extends")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "extends");
-                }
-                if (palavra.toString().equals("final")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "final");
-                }
-                if (palavra.toString().equals("finally")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "finally");
-                }
-                if (palavra.toString().equals("float")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "float");
-                }
-                if (palavra.toString().equals("for")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "for");
-                }
-                if (palavra.toString().equals("goto")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "goto");
-                }
-                if (palavra.toString().equals("if")) {
-                       return new Token(TipodeToken.PALAVRA_RESERVADA, "if");
-                }
-                if (palavra.toString().equals("implements")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "implements");
-                }
-                if (palavra.toString().equals("import")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "import");
-                }
-                if (palavra.toString().equals("instanceof")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "instanceof");
-                }
-                if (palavra.toString().equals("int")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "int");
-                }
-                if (palavra.toString().equals("interface")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "interface");
-                }
-                if (palavra.toString().equals("long")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "long");
-                }
-                if (palavra.toString().equals("native")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "native");
-                }
-                if (palavra.toString().equals("new")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "new");
-                }
-                if (palavra.toString().equals("null")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "null");
-                }
-                if (palavra.toString().equals("package")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "package");
-                }
-                if (palavra.toString().equals("private")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "private");
-                }
-                if (palavra.toString().equals("protected")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "protected");
-                }
-                if (palavra.toString().equals("public")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "public");
-                }
-                if (palavra.toString().equals("return")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "return");
-                }
-                if (palavra.toString().equals("short")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "short");
-                }
-                if (palavra.toString().equals("static")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "static");
-                }
-                if (palavra.toString().equals("strictfp")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "strict");
-                }
-                if (palavra.toString().equals("super")) {
-                    return new Token(TipodeToken.PALAVRA_RESERVADA, "super");
-                }
-                if (palavra.toString().equals("switch")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"switch");
-                }
-                if (palavra.toString().equals("synchronized")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"synchronized");
-                }
-                if (palavra.toString().equals("this")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"this");
-                }
-                if (palavra.toString().equals("throw")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"throw");
-                }
-                if (palavra.toString().equals("throws")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"throws");
-                }
-                if (palavra.toString().equals("transient")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"transient");
-                }
-                if (palavra.toString().equals("try")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"try");
-                }
-                if (palavra.toString().equals("void")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"void");
-                }
-                if (palavra.toString().equals("volatile")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"volatile");
-                }
-                if (palavra.toString().equals("while")){
-                    return new Token(TipodeToken.PALAVRA_RESERVADA,"while");
-                }
-
-                //Operadores aritmeticos
-               if (c == '*') {
-                    return new Token(TipodeToken.OPERADOR_ARITIMETICO_MULTIPLICACAO, "*");
-                }
-               if (c == '+') {
-                    return new Token(TipodeToken.OPERADOR_ARITIMETICO_SOMA, "+");
-                }
-               if (c == '-') {
-                    return new Token(TipodeToken.OPERADOR_ARITMETICO_SUBTRACAO, "-");
-                }
-               if (c == '/') {
-                   return new Token(TipodeToken.OPERADOR_ARITMETICO_DIVISAO, "/");
-               }
-               if(c == '%'){
-                   return new Token(TipodeToken.OPERADOR_ARITMETICO_RESTO, "%");
-               }
-               if (palavra.toString().equals("++")){
-                   return new Token(TipodeToken.OPERADOR_ARITMETICO_INCREMENTO, "++");
-               }
-                if (palavra.toString().equals("--")){
-                    return new Token(TipodeToken.OPERADOR_ARITMETICO_DECREMENTO, "--");
-                }
-
-
-
-
-                //OPERADORES RELACIONAIS, ATRIBUIÇÃO
-                if (c == '='){
-                    return new Token(TipodeToken.IGUAL, "=");
-                }
-                if (palavra.toString().equals("+=")){
-                    return new Token(TipodeToken.SOMA_IGUAL, "+=");
-                }
-                if (palavra.toString().equals("-=")){
-                    return new Token(TipodeToken.SOMA_IGUAL, "-=");
-                }
-                if (c == '<'){
-                    return new Token(TipodeToken.MENORQUE, "<");
-                }
-                if (c == '>'){
-                    return new Token(TipodeToken.MAIORQUE, ">");
-                }
-                if (palavra.toString().equals("<=")){
-                    return new Token(TipodeToken.MENORQUE_IGUAL, "<=");
-                }
-                if (palavra.toString().equals(">=")){
-                    return new Token(TipodeToken.MAIORQUE_IGUAL, ">=");
-                }
-
-
-                //Operadores Lógicos
-
-                //OPERADORES BIT A BIT
-
-                //OPERADOR TERNARIO
-
-                if (c == ':') {
-                    return new Token(TipodeToken.DOIS_PONTOS, ":");
-                }
-                if (c == '.') {
-                    return new Token(TipodeToken.PONTO, ".");
-                }
-                if (c == ',') {
-                    return new Token(TipodeToken.VIRGULA, ",");
-                }
-                }
-                if (c == '<') {
-
-                }
+                return verificarIdentificador(palavra.toString());
             }
 
-        return null;
+            // Identificando delimitadores
+            switch (c) {
+                case '(': return new Token(TipodeToken.PARENTESE_ABRINDO, "(");
+                case ')': return new Token(TipodeToken.PARENTESE_FECHANDO, ")");
+                case '{': return new Token(TipodeToken.CHAVES_ABRINDO, "{");
+                case '}': return new Token(TipodeToken.CHAVES_FECHANDO, "}");
+                case '[': return new Token(TipodeToken.CHAVES_ABRINDO, "[");
+                case ']': return new Token(TipodeToken.CHAVES_FECHANDO, "]");
+                case ';': return new Token(TipodeToken.PONTO_VIRGULA, ";");
+                case ':': return new Token(TipodeToken.DOIS_PONTOS, ":");
+                case ',': return new Token(TipodeToken.VIRGULA, ",");
+                case '.': return new Token(TipodeToken.PONTO, ".");
+            }
+
+            // Identificando operadores aritméticos
+            switch (c) {
+                case '*': return new Token(TipodeToken.OPERADOR_ARITIMETICO_MULTIPLICACAO, "*");
+                case '+': return new Token(TipodeToken.OPERADOR_ARITIMETICO_SOMA, "+");
+                case '-': return new Token(TipodeToken.OPERADOR_ARITMETICO_SUBTRACAO, "-");
+                case '/': return new Token(TipodeToken.OPERADOR_ARITMETICO_DIVISAO, "/");
+                case '%': return new Token(TipodeToken.OPERADOR_ARITIMETICO_RESTO, "%");
+                case '=': return new Token(TipodeToken.IGUAL, "=");
+                case '<': return new Token(TipodeToken.MENORQUE, "<");
+                case '>': return new Token(TipodeToken.MAIORQUE, ">");
+            }
+
+            // Adicione mais operadores e lógicas conforme necessário
         }
-
-
+        return null; // Se não há mais tokens
     }
 
+    private Token verificarIdentificador(String valor) {
+        // Regex para identificadores válidos
+        String regex = "^[a-zA-Z_][a-zA-Z0-9_]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(valor);
 
+        if (matcher.matches()) {
+            // Verifica se é uma palavra reservada
+            if (isPalavraReservada(valor)) {
+                return new Token(TipodeToken.PALAVRA_RESERVADA, valor);
+            }
+            return new Token(TipodeToken.IDENTIFICADOR, valor);
+        }
+        return null; // Ou trate de outra forma se necessário
+    }
 
+    private boolean isPalavraReservada(String palavra) {
+        // Verifica se a palavra é uma palavra reservada
+        // Coloque aqui a lógica para verificar as palavras reservadas
+        String[] palavrasReservadas = {
+                "abstract", "assert", "boolean", "break", "case", "byte",
+                "catch", "char", "class", "const", "continue", "default",
+                "do", "double", "else", "enum", "extends", "final",
+                "finally", "float", "for", "goto", "if", "implements",
+                "import", "instanceof", "int", "interface", "long",
+                "native", "new", "null", "package", "private", "protected",
+                "public", "return", "short", "static", "strictfp",
+                "super", "switch", "synchronized", "this", "throw",
+                "throws", "transient", "try", "void", "volatile", "while"
+        };
+
+        for (String p : palavrasReservadas) {
+            if (palavra.equals(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
