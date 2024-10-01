34e96a96552097921b82f31935f2b13200ca8711
Joaobatiist <joaovpbo@outlook.com>
2024-10-01 18:20:07 -0300

    Arrumaçao do literal Numb

--- Compilador/src/main/java/org/example/compilador/LerToken.java
+++ Compilador/src/main/java/org/example/compilador/LerToken.java
@@ -32,7 +32,11 @@ public class LerToken {
         while ((caractereLido = ldat.lerProximoCaractere()) != -1) {
             char c = (char) caractereLido;
 
+
+
+
             if (c == ' ' || c == '\n' ) continue;
+
 
             if (c == '/') {
                 int nextChar = ldat.lerProximoCaractere();
@@ -73,6 +77,7 @@ public class LerToken {
             }
 
 
+
             if (Character.isLetter(c) || c == '_') {
                 palavra.append(c);
                 while ((caractereLido = ldat.lerProximoCaractere()) != -1) {
@@ -94,6 +99,7 @@ public class LerToken {
                     }
                 }
                 return verificarIdentificador(palavra.toString());
+
             }
             if (Character.isDigit(c)) {
                 palavra.append(c);
@@ -109,19 +115,6 @@ public class LerToken {
                 return verificarNumero(palavra.toString());
             }
 
-            if (Character.isDigit(c)) {
-                palavra.append(c);
-                while ((caractereLido = ldat.lerProximoCaractere()) != -1) {
-                    c = (char) caractereLido;
-                    if (Character.isDigit(c)) {
-                        palavra.append(c);
-                    } else {
-                        ldat.retroceder(caractereLido);
-                        break;
-                    }
-                }
-                return verificarNumero(palavra.toString());
-            }
 
             // Identificando delimitadores
             switch (c) {
@@ -169,9 +162,8 @@ public class LerToken {
         }
         return null; // Se não há mais tokens
     }
-
+    int contadorIdentificadores = 0;
     private Token verificarIdentificador(String valor) {
-        int contadorIdentificadores = 0;
         // Regex para identificadores válidos
         String regex = "^[a-zA-Z_][a-zA-Z0-9_]*$";
         Pattern pattern = Pattern.compile(regex);
@@ -186,17 +178,6 @@ public class LerToken {
             return new Token(TipodeToken.IDENTIFICADOR, valor);
         }
 
-        return null;
-    }
-
-    private Token verificarNumero(String valor){
-        String numberRegex = "^[0-9]*$";
-        Pattern paternn = Pattern.compile(numberRegex);
-        Matcher matcher2 = paternn.matcher(valor);
-
-        if (matcher2.matches()){
-            return new Token(TipodeToken.NUMB, valor);
-        }
         return null;
     }
 
